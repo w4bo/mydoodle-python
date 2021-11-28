67f36f81e6ee67240771cf79c3ba7db30b29bca4
w4bo <m.francia@unibo.it>
2021-11-28 22:29:41 +0100

    update

---
+++ doodle.py
@@ -0,0 +1,116 @@
+"""
+Python wrapper for the Doodle API
+GET, no POST
+"""
+from datetime import datetime, timedelta, timezone
+import json
+import urllib.parse
+import requests
+from datetime import datetime
+import pytz
+from pprint import pprint
+
+class Doodle:
+    """Connect with Doodle"""
+
+    def __init__(self, url=None, poll_id=None):
+        assert url or poll_id
+        if not poll_id:
+            parsed = urllib.parse.urlparse(url)
+            poll_id = parsed.path.replace('/', '').replace('poll', '')
+        self.base_url = f"https://doodle.com/api/v2.0/polls/{poll_id}?adminKey=&participantKey="
+        self.url = url if url else f"https://doodle.com/poll/{poll_id}"
+        self.json_file = None
+        self.update()
+        tz = self.json_file['initiator'].get('timeZone')  # sometimes the api provides a tz,
+                                                          # sometimes it doesn't,
+                                                          # not sure why
+        self.timezone = pytz.timezone(tz) if tz else timezone(timedelta(hours=-12))  # todo -12 is system specific
+
+
+    def update(self, url: str=None):
+        """Send a request to Doodle"""
+        if not url:
+            url = self.base_url
+        req = requests.request('get', url)
+        if req.status_code == 200:
+            self.json_file = json.loads(req.text)
+            pprint(self.json_file)
+        elif req.status_code == 404:
+            return
+        else:
+            print(url)
+            print(req.status_code)
+            print(req.text)
+            raise ConnectionError
+
+
+    @property
+    def options(self) -> list:
+        def t2d(timestamp):
+            return datetime.fromtimestamp(timestamp)
+        return [(t2d(p['date'] / 1000), t2d(p['start'] / 1000), p['text']) for p in self.json_file["options"]]
+
+
+    @property
+    def json(self) -> str:
+        return self.json_file
+
+
+    @property
+    def participants(self) -> list:
+        return [(p['name'], p['preferences']) for p in self.json_file["participants"]]
+
+    @property
+    def title(self) -> str:
+        return self.json_file['title']
+
+    @property
+    def location(self) -> str or None:
+        location = self.json_file.get('location')
+        if location:
+            return location['name']
+
+    @property
+    def description(self) -> str or None:
+        return self.json_file.get('description')
+
+    @property
+    def comments(self) -> list:
+        return self.json_file.get('comments')
+
+    @property
+    def initiator(self) -> str:
+        return self.json_file["initiator"][0]['name']
+
+    @property
+    def latest_change(self) -> datetime:
+        return datetime.fromtimestamp(self.json_file["latestChange"]/1000, tz=self.timezone)
+
+    @property
+    def final(self) -> list:
+        options = self.json_file.get('options')
+        result = []
+        if options:
+            for o in options:
+                if o.get('final'):
+                    dt_start = None
+                    dt_end = None
+                    try:
+                        dt_start = datetime.fromtimestamp(o.get('start')/1000, tz=self.timezone)
+                    except ValueError:
+                        pass
+                    try:
+                        dt_end = datetime.fromtimestamp(o.get('end')/1000, tz=self.timezone)
+                    except (ValueError, TypeError):
+                        pass
+                    if dt_start or dt_end:
+                        result.append((dt_start, dt_end))
+            return result
+
+    @property
+    def is_open(self) -> bool:
+        if self.json_file['state'] == 'OPEN':
+            return True
+        else:
+            return False
---
+++ read.py
@@ -0,0 +1,23 @@
+from doodle import Doodle
+import pandas as pd
+import datetime as dt
+import re 
+
+d = Doodle(url="https://doodle.com/poll/6c9nrmyeyt5gcih3")
+# d = Doodle(url="https://doodle.com/poll/6a275rvfdyuqmfux")
+df = pd.DataFrame(d.options, columns=["date", "start", "text"])
+df["week"] = df["date"].apply(lambda x: (x + dt.timedelta(days=1)).week)
+df['day'] = df[['date']].apply(lambda x: dt.datetime.strftime(x['date'], '%A'), axis=1)
+i = 0
+for p, preferences in d.participants:
+    df["p_" + str(i) + p] = preferences
+    df["p_" + str(i) + p] = df["p_" + str(i) + p].apply(lambda x: p if x == 1 else "")
+    i += 1
+df["curweek"] = dt.datetime.now()
+df["curweek"] = df["curweek"].apply(lambda x: (x + dt.timedelta(days=1)).week)
+df = df[df["curweek"] == df["week"]]
+df = df[["date", "week", "day", "text"] + [x for x in df.columns if "p_" in x]]
+df["s"] = df[[x for x in df.columns if "p_" in x]].agg(', '.join, axis=1).apply(lambda x: re.sub(' +', ' ', x.replace(',', '')).strip())
+df = df[df["s"].apply(lambda x: x != "")]
+print(df[["week", "date", "day", "text", "s"]].to_markdown())
+df.apply(lambda x: str(x["date"].strftime("%d/%m/%Y")) + " " +  x["text"] + " " + x["s"], axis=1).to_csv("README.md", index=False, header=False)
